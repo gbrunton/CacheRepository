0ef140058aeb0c377d6f55c12b347546a9c09869
Gary Brunton <gbrunton@gmail.com>
2013-09-13 11:28:47 -0700

    fixing release notes indentation
    making sure the nuget pack is calling the project file not the nuspec file.

--- createNugetPackageAndPush.bat
+++ createNugetPackageAndPush.bat
@@ -2,7 +2,7 @@ REM Update the following files before running this:
 REM ./CacheRepository/CacheRepository.nuspec Add new release note
 REM ./CacheRepository/Properties/AssemblyInfo.cs Update AssemblyVersion attribute
 
-"./.nuget/nuget" pack ./CacheRepository/CacheRepository.nuspec
+"./.nuget/nuget" pack ./CacheRepository/CacheRepository.csproj
 
 "./.nuget/nuget" push *.nupkg
 
