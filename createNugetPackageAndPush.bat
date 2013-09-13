ce4d42d038dec00303808b7dcbf2a77ceac9b519
Gary Brunton <gbrunton@gmail.com>
2013-09-13 11:21:56 -0700

    Updated the nuspec and created the createNugetPackageAndPush.bat file that I should use in the future to push updates to nuget.

---
+++ createNugetPackageAndPush.bat
@@ -0,0 +1,9 @@
+REM Update the following files before running this:
+REM ./CacheRepository/CacheRepository.nuspec Add new release note
+REM ./CacheRepository/Properties/AssemblyInfo.cs Update AssemblyVersion attribute
+
+"./.nuget/nuget" pack ./CacheRepository/CacheRepository.nuspec
+
+"./.nuget/nuget" push *.nupkg
+
+pause
