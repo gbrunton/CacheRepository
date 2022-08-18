0d3d64a78d9d3a4c2b101759123da2e9fdc55e78
GaryBrunton <gbrunton@ssatpa.net>
2022-08-17 17:04:08 -0700

    issue 18:
    Updating project dependencies.
    
    Replaced Dapper.Extensions with SqlKata.
    
    Updated to .net 4.8

---
+++ Tests/Fakes/ExecuteSqlStrategyFake.cs
@@ -0,0 +1,16 @@
+﻿using CacheRepository.ExecuteSqlStrategies;
+
+namespace Tests.Fakes
+{
+    public class ExecuteSqlStrategyFake : IExecuteSqlStrategy
+    {
+        public void Execute(string sql, object parameters)
+        {
+            this.Sql = sql;
+            this.Parameters = parameters;
+        }
+
+        public string Sql { get; private set; }
+        public object Parameters { get; private set; }
+    }
+}
---
+++ Tests/UnitTests/InsertStrategies/SqlInsertWithDapperTestFixture.cs
@@ -0,0 +1,68 @@
+﻿using CacheRepository.InsertStrategies;
+using NUnit.Framework;
+using Tests.Entities;
+using Tests.Fakes;
+
+namespace Tests.UnitTests.InsertStrategies
+{
+    // These tests are very much dependent on the sql the SqlKata generates which is not ideal. It would be better
+    // if the queries were executed against a real db but I'm not going to integrate that right now.
+    public class SqlInsertWithDapperTestFixture
+    {
+        [TestFixture]
+        public class When_inserting : SqlInsertWithDapperTestFixture
+        {
+            private ExecuteSqlStrategyFake executeSqlStrategy;
+
+            private SqlInsertWithDapper getSut()
+            {
+                return new SqlInsertWithDapper(this.executeSqlStrategy);
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                this.executeSqlStrategy = new ExecuteSqlStrategyFake();
+            }
+
+            [Test]
+            public void the_sql_is_generated_correctly()
+            {
+                // Arrange
+                var sut = this.getSut();
+                var blog = new Blog
+                {
+                    Id = 1,
+                    Author = "Gary Brunton",
+                    Title = "Update Test"
+                };
+
+                // Act
+                sut.Insert(blog);
+
+                // Assert
+                Assert.AreEqual("INSERT INTO [Blog] ([Id], [Title], [Author], [CreatedDate], [ModifiedDate]) VALUES (@p0, @p1, @p2, @p3, @p4)", this.executeSqlStrategy.Sql);
+            }
+
+            [Test]
+            public void and_multiple_updates_are_called_the_sql_is_generated_correctly()
+            {
+                // Arrange
+                var sut = this.getSut();
+                var blog = new Blog
+                {
+                    Id = 1,
+                    Author = "Gary Brunton",
+                    Title = "Update Test"
+                };
+                sut.Insert(blog);
+
+                // Act
+                sut.Insert(blog);
+
+                // Assert
+                Assert.AreEqual("INSERT INTO [Blog] ([Id], [Title], [Author], [CreatedDate], [ModifiedDate]) VALUES (@p0, @p1, @p2, @p3, @p4)", this.executeSqlStrategy.Sql);
+            }
+        }
+    }
+}
